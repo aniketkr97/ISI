e755b60f491b4bb3cc2357392674079125801009
Aniket Kumar <32923119+aniketkr97@users.noreply.github.com>
2022-09-19 16:04:19 -0700

    Query 2

---
+++ Q2.sql
@@ -0,0 +1,19 @@
+// Query about such a video which has a Person 1 (Alex) and Person 2(Helen) talking about cellular component X
+
+SELECT
+	V.video_id,
+    M.media_name,
+	C.cellular_component_name
+    P.person_id
+
+FROM Media M
+INNER JOIN Cellular_Components C   ON (M.pdb_id = C.pdb_id)
+INNER JOIN Video V  ON (M.video_id = V.video_id)
+INNER JOIN Person P ON (M.person_id = )
+
+WHERE C.cellular_component_name LIKE '%Insulin%'
+WHERE P.person_name = 'Alex' OR P.person_name = 'Helen'
+
+GROUP BY
+    P.person
+	
