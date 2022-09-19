5363384d8ed4d8ed1a946096be19e30ee58e867a
Aniket Kumar <32923119+aniketkr97@users.noreply.github.com>
2022-09-19 15:54:07 -0700

    Add files via upload

---
+++ Q1.sql
@@ -0,0 +1,15 @@
+// Query to Select Videos which have a media component in them. 
+SELECT
+	V.video_id,
+    M.media_name,
+	C.pdb_id
+
+FROM Media M
+INNER JOIN Cellular_Components C   ON (M.pdb_id = C.pdb_id)
+INNER JOIN Video V  ON (M.video_id = V.video_id)
+
+WHERE C.cellular_component_name LIKE '%Insulin%'
+
+GROUP BY
+	V.video_id,
+	
